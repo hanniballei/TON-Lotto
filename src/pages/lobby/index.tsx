1ed3982a43ce2328866562f1f9c5c3e3c0c20815
Dnevend <Dnevend@gmail.com>
2024-05-18 14:27:51 +0800

    styles: lobby

--- src/pages/lobby/index.tsx
+++ src/pages/lobby/index.tsx
@@ -1,9 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { CircleX } from "lucide-react";
+import { useCountUp } from "react-countup";
+import LobbyBox from "@/assets/lobby-box.png";
 
 const Lobby = () => {
   const [prizeValue, setPrizeValue] = useState<string[]>([]);
   const [scratchedPercent, setScratchedPercent] = useState(0);
+
+  const congratsDialog = useRef<HTMLDialogElement>(null);
+  const countUpRef = useRef<HTMLSpanElement>(null);
+
+  const { start } = useCountUp({
+    ref: countUpRef,
+    start: 0,
+    end: prizeValue.filter((it) => it === "🐸").length * 1000,
+    delay: 0,
+    duration: 1,
+  });
 
   useEffect(() => {
     initializeCanvas();
@@ -11,9 +25,11 @@ const Lobby = () => {
 
   useEffect(() => {
     if (scratchedPercent === 100) {
-      // TODO:弹窗提示中奖额度
+      // 中奖提示
+      congratsDialog.current?.showModal();
+      start();
     }
-  }, [scratchedPercent]);
+  }, [scratchedPercent, start]);
 
   const initializeCanvas = () => {
     const canvasElement = document.getElementById(
@@ -27,11 +43,39 @@ const Lobby = () => {
     canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
     canvasContext.globalCompositeOperation = "source-over";
 
+    // 填充涂层背景色
     const gradient = canvasContext.createLinearGradient(0, 0, 135, 135);
     gradient.addColorStop(0, "#d63031");
     gradient.addColorStop(1, "#fdcb6e");
     canvasContext.fillStyle = gradient;
     canvasContext.fillRect(0, 0, 320, 320);
+
+    // 填充涂层默认图片
+    const image = new Image();
+    image.src = LobbyBox;
+    image.onload = function () {
+      const cols = 4; // 列数
+      const rows = 3; // 行数
+      const imageWidth = 60; // 固定图片宽度
+      const imageHeight = 60; // 固定图片高度
+
+      const cellWidth = canvasElement.width / cols;
+      const cellHeight = canvasElement.height / rows;
+
+      for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
+          const centerX = x * cellWidth + (cellWidth - imageWidth) / 2;
+          const centerY = y * cellHeight + (cellHeight - imageHeight) / 2;
+          canvasContext.drawImage(
+            image,
+            centerX,
+            centerY,
+            imageWidth,
+            imageHeight
+          );
+        }
+      }
+    };
 
     // 初始化刮刮乐随机值
     setPrizeValue(
@@ -149,8 +193,8 @@ const Lobby = () => {
       const percent = (transparentPixels / totalPixels) * 100;
       setScratchedPercent(percent);
 
-      // 当刮出百分比超过80时自动开奖
-      if (percent > 80) {
+      // 当刮出百分比超过75%时自动开奖
+      if (percent > 75) {
         revealAll();
       }
     };
@@ -160,24 +204,37 @@ const Lobby = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center gap-6">
-      <div className="relative rounded-md w-[320px] h-[320px]">
-        <div className="w-full h-full grid grid-cols-4">
-          {prizeValue.map((it) => (
-            <div className="border flex justify-center items-center">{it}</div>
-          ))}
+    <>
+      <div className="flex flex-col justify-center items-center gap-6">
+        <div className="relative rounded-md w-[320px] h-[320px] border-4 border-yellow-300 overflow-hidden">
+          <div className="w-full h-full grid grid-cols-4">
+            {prizeValue.map((it) => (
+              <div className="flex justify-center items-center">{it}</div>
+            ))}
+          </div>
+          <canvas
+            id="scratch"
+            width="320"
+            height="320"
+            className="absolute inset-0 w-full h-full text-center select-none"
+          ></canvas>
         </div>
-        <canvas
-          id="scratch"
-          width="320"
-          height="320"
-          className="absolute inset-0 w-full h-full text-center select-none"
-        ></canvas>
+
+        <div className="font-bold">Percent {scratchedPercent.toFixed(2)} %</div>
+        <Button onClick={initializeCanvas}>Restart</Button>
       </div>
 
-      <div className="font-bold">Percent {scratchedPercent.toFixed(2)} %</div>
-      <Button onClick={initializeCanvas}>Restart</Button>
-    </div>
+      <dialog ref={congratsDialog} className=" bg-transparent">
+        <div className="flex flex-col gap-4 justify-center items-center p-6 rounded-md bg-white">
+          <p className="text-lg">
+            You got <span ref={countUpRef} /> 🐸 Points
+          </p>
+          <Button autoFocus onClick={() => congratsDialog.current?.close()}>
+            <CircleX />
+          </Button>
+        </div>
+      </dialog>
+    </>
   );
 };
 
