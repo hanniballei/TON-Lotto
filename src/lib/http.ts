db737c2e219df7df0b73061646722603a45d33b8
Dnevend <Dnevend@gmail.com>
2024-06-15 15:23:46 +0800

    feat: toast http error

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,11 +1,14 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 import { storageGet } from "./storage"
+import { toast } from "@/components/ui/use-toast"
 
 const RequestUrl = {
     dev: 'http://localhost:5008',
     // TODO: change when publish
     prod: 'https://api-lotto-test.smaug.cc'
 }
+
+// const mockUserDataRaw = 'user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3788475317572404878&chat_type=private&auth_date=1709144340&hash=371697738012ebd26a111ace4aff23ee265596cd64026c8c3677956a85ca1827'
 
 export const http = axios.create({
     baseURL: RequestUrl[import.meta.env.DEV ? 'dev' : 'prod'],
@@ -16,7 +19,6 @@ http.interceptors.request.use((req) => {
 
     if (storageGet('init_data_raw')) {
         req.headers['tma-authorization'] = `tma ${storageGet('init_data_raw')}`
-        // req.headers['tma-authorization'] = `tma ${'user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3788475317572404878&chat_type=private&auth_date=1709144340&hash=371697738012ebd26a111ace4aff23ee265596cd64026c8c3677956a85ca1827'}`
     }
 
     return req
@@ -24,4 +26,6 @@ http.interceptors.request.use((req) => {
 
 http.interceptors.response.use((res) => {
     return res.data
+}, (err: AxiosError) => {
+    toast({ variant: 'destructive', description: err.message })
 })
