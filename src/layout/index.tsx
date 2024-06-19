72ab7e5076ea0754bf9960953a817d93c576fa95
Dnevend <Dnevend@gmail.com>
2024-06-20 00:34:28 +0800

    styles: layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -115,8 +115,8 @@ const Layout = () => {
   return (
     <>
       <BackButtonManipulator />
-      <div className="bg-gradient-to-r from-[#0a39b0] to-blue-950">
-        <div className={"container min-h-lvh max-w-lg flex flex-col px-4"}>
+      <div className="bg-gradient-to-r min-h-lvh from-[#0a39b0] to-blue-950">
+        <div className={"container max-w-lg flex flex-col px-4"}>
           <Header />
           <main className="relative grow">
             <Suspense
